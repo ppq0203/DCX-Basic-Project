8e36137dbd421116dc5788c08e2363aee3d7cbee
JunSeop Lee <ppq23@naver.com>
2023-03-03 16:17:45 +0900

    test setting

--- src/main/java/project/shop/controller/UserController.java
+++ src/main/java/project/shop/controller/UserController.java
@@ -18,14 +18,14 @@ public class UserController {
 	@Autowired
     private UserService userService; //서비스와 연결
 	
-	@GetMapping("/test") //노테이션의 값으로 주소 지정
-    public String openBoardList() throws Exception{
-    	//templates 폴더 아래있는 /boardList.html을 의미함. Thymeleaf와 같은 템플릿엔진을 사용할 경우 스프링 부트의 자동 설정 기능으로 '.html'과 같은 접미사 생략 가능
-    	System.out.println("/test");
-    	System.out.println(userService.selectUserList());
-        //게시글 목록을 조회하기 위해 ServiceImpl 클래스의 selectBoardList 메서드 호출
-        return "/boardWrite";
-    }
+//	@GetMapping("/test") //노테이션의 값으로 주소 지정
+//    public String openBoardList() throws Exception{
+//    	//templates 폴더 아래있는 /boardList.html을 의미함. Thymeleaf와 같은 템플릿엔진을 사용할 경우 스프링 부트의 자동 설정 기능으로 '.html'과 같은 접미사 생략 가능
+//    	System.out.println("/test");
+//    	System.out.println(userService.selectUserList());
+//        //게시글 목록을 조회하기 위해 ServiceImpl 클래스의 selectBoardList 메서드 호출
+//        return "/boardWrite";
+//    }
 	
 	@GetMapping("/login")
 	public String loginPage()
@@ -54,7 +54,7 @@ public class UserController {
 	}
 	
 	@PostMapping("/postJoin")
-	public String postJoin()
+	public String postJoin(UserDto userDto)
 	{
 		return "redirect:/login";
 	}
