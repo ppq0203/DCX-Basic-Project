349a6a563b15e5d0b426a8f2a3fa25601cd1dea3
SMchoi <fpahsgkdla5421@gmail.com>
2023-03-06 11:32:25 +0900

    findUser Using If

--- src/main/java/project/shop/controller/UserController.java
+++ src/main/java/project/shop/controller/UserController.java
@@ -35,12 +35,11 @@ public class UserController {
 	
 	
 	@PostMapping("/postLogin")		//작성된 게시글 등록 기능 메소드, html의 form 태그 action에서 입력한 주소
-    public String postLogin(HttpSession session, UserDto user, 
-    		@RequestParam(value = "userId") String chkId) throws Exception{
+    public String postLogin(HttpSession session, UserDto user) throws Exception{
     	System.out.println("postLogin::"+user);
     	UserDto getUserDto = userService.findUser(user);	//ID 정보로 유저정보 확인
     	System.out.println("post" + getUserDto);	//유저정보 제대로 받아왔는지 확인
-    	if (getUserDto != null && getUserDto.getUserId() == chkId)
+    	if (getUserDto != null && getUserDto == user)
     	{
     		session.setAttribute("userDto", getUserDto);	//세션에 유저정보 저장
     		return "/";	//메인으로 이동
