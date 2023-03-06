7c6cd1dd5d5d67fa4e0556a7b100edca67151877
JunSeop Lee <ppq23@naver.com>
2023-03-06 10:13:44 +0900

    Postlogin rename
    loginUser -> findUser

--- src/main/java/project/shop/controller/UserController.java
+++ src/main/java/project/shop/controller/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
 	@PostMapping("/postLogin")		//작성된 게시글 등록 기능 메소드, html의 form 태그 action에서 입력한 주소
     public String postLogin(HttpSession session, UserDto user) throws Exception{
     	System.out.println("postLogin::"+user);
-    	UserDto getUserDto = userService.loginUser(user);	//ID 정보로 유저정보 확인
+    	UserDto getUserDto = userService.findUser(user);	//ID 정보로 유저정보 확인
     	System.out.println("post" + getUserDto);	//유저정보 제대로 받아왔는지 확인
     	if (getUserDto != null && getUserDto == user)
     	{
