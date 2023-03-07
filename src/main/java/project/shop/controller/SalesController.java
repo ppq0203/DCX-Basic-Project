0a030299ca5fb0642e2a0ae37c6e0adead9e567e
SMchoi <fpahsgkdla5421@gmail.com>
2023-03-07 10:48:33 +0900

    상품 등록 쿼리 논리오류 수정 및 salesDto 불러오는 쿼리문 작

--- src/main/java/project/shop/controller/SalesController.java
+++ src/main/java/project/shop/controller/SalesController.java
@@ -24,28 +24,19 @@ public class SalesController {
 	public String addprodPage(@ModelAttribute SalesDto sales, Model model)
 	{
 		model.addAttribute("Sales", sales);
-		return "/productWrite";
+		return "/dbtest2";
 	}
 		
-	//상품목록 페이지
 	@GetMapping("/showprod")
-	public String prodPage()
-	{
-		return "/dbtest";
-	}
-	
-	@PostMapping("/showprod")
 	public ModelAndView prodPage(SalesDto sales) throws Exception
 	{
-		//templates 폴더 아래있는 /boardList.html을 의미함. Thymeleaf와 같은 템플릿엔진을 사용할 경우 스프링 부트의 자동 설정 기능으로 '.html'과 같은 접미사 생략 가능
-				System.out.println("/showprod");
-		    	ModelAndView mv = new ModelAndView("/dbtest"); 
-		        //게시글 목록을 조회하기 위해 ServiceImpl 클래스의 selectBoardList 메서드 호출
-		        List<SalesDto> list = salesService.selectProdList(sales);  
-		        mv.addObject("list", list);
-				System.out.println("found");
-				
-				return mv;
+		System.out.println("/showprod");
+    	ModelAndView mv = new ModelAndView("/dbtest");
+        List<SalesDto> list = salesService.selectProdList(sales);  
+        mv.addObject("list", list);
+		System.out.println("found");
+		
+		return mv;
 	}
 	
 	@PostMapping("insertProduct")
@@ -53,6 +44,6 @@ public class SalesController {
 	{
 		System.out.println(" [+] post "+sales);
 		salesService.insertProduct(sales);
-		return "redirect:/main";
+		return "redirect:/showprod";
 	}
 }
